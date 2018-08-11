332897b915cd4543b7d80f688fbc82af126b53f3
Yogesh Agarwal <>
2018-08-11 14:39:36 +0100

    Added list item

---
+++ src/app/listitem/listitem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listitem',
+  templateUrl: './listitem.component.html',
+  styleUrls: ['./listitem.component.css']
+})
+export class ListitemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
